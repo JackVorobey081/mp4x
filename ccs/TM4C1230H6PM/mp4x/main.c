78ba9138556f9e0456ed6da5d80e751dba6827a4
GitHubTester081 <JackVorobey081@gmail.com>
2023-10-10 15:18:24 +0300

    настройка проекта mp4x

--- ccs/TM4C1230H6PM/mp4x/main.c
+++ ccs/TM4C1230H6PM/mp4x/main.c
@@ -1,9 +1,52 @@
 
+#include <stdbool.h>
+#include <stdint.h>
+#include <string.h>
+
+#include "inc/hw_nvic.h"
+#include "inc/hw_ints.h"
+#include "inc/hw_types.h"
+#include "inc/hw_ints.h"
+#include "inc/hw_memmap.h"
+#include "inc/hw_can.h"
+
+#include "driverlib/rom.h"
+#include "driverlib/ssi.h"
+#include "driverlib/can.h"
+#include "driverlib/gpio.h"
+#include "driverlib/pin_map.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/interrupt.h"
+#include "driverlib/systick.h"
+#include "driverlib/rom_map.h"
 
 /**
  * main.c
  */
 int main(void)
 {
-	return 0;
+    //
+    // Set the system clock to run at 50MHz from the PLL
+    //
+    MAP_SysCtlClockSet(SYSCTL_SYSDIV_4 | SYSCTL_USE_PLL | SYSCTL_XTAL_24MHZ |
+                       SYSCTL_OSC_MAIN);
+
+    //
+    // Initialize CAN0
+    //
+    //InitCAN0();
+
+    //
+    // Transmit message CAN0
+    //
+    //CAN0Transmit();
+
+
+    //---------------------------------------------------------------//
+
+    while(1)
+    {
+
+
+    }
 }
